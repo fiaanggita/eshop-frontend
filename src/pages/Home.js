da76763be8c00a4f2797ea1c199213f92b35629f
Fia Anggita <nengfiaanggita26@gmail.com>
2022-07-21 15:31:41 +0700

    Membuat List Produk

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CardList from "../components/CardList";
+import { Container } from 'react-bootstrap';
+
+function Home() {
+    return (
+        <Container>
+            <h1> Home </h1>
+            <CardList />
+        </Container>
+    );
+
+};
+
+export default Home;
